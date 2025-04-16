d4f5f7ec9e9d4b433e51b09dca5cc03b1bd5077f
catiradocortes <carlostiradocortes@gmail.com>
2025-04-16 17:35:45 +1000

    activity 1 implemented
    I finished the first resize activiy

--- IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
+++ IDEA_9103_WK_7_Complete/IDEA_9103_WK_7_Complete/sketch.js
@@ -10,11 +10,6 @@ let segments = [];
 //lets add a variable to switch between drawing the image and the segments
 let drawSegments = true;
 
-let imgDrawProperties = {aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0};
-
-let canvasAspectRatio = 0;
-
-
 //lets load the image from disk
 function preload() {
   img = loadImage('/assets/Mona_Lisa_by_Leonardo_da_Vinci_500_x_700.jpg');
@@ -22,16 +17,7 @@ function preload() {
 
 function setup() {
   //We will make the canvas the same size as the image using its properties
-  createCanvas(windowWidth, windowHeight); // !Here we have 1 change
-
-  // ! calculate the aspect ratio
-  imgDrawProperties.aspect = img.width / img.height;
-
-  //! canvas aspect ratio
-  canvasAspectRatio = width/height;
-  //! Calculate the aspect ratio fo the canvas
-  calculateImageDrawProperties();
-
+  createCanvas(img.width, img.height);
   //We can use the width and height of the image to calculate the size of each segment
   let segmentWidth = img.width / numSegments;
   let segmentHeight = img.height / numSegments;
@@ -45,8 +31,8 @@ function setup() {
       //We will use the x and y position to get the colour of the pixel from the image
       //lets take it from the centre of the segment
       let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
-      let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
-      segments.push(segment);
+       let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
+       segments.push(segment);
     }
   }
 }
@@ -60,11 +46,9 @@ function draw() {
     }
   } else {
     //lets draw the image to the canvas
-    image(img, imgDrawProperties.xOffset, imgDrawProperties.yOffset, imgDrawProperties.width, imgDrawProperties.height);
+    image(img, 0, 0);
   }
 }
-
-
 function keyPressed() {
   if (key == " ") {
     //this is a neat trick to invert a boolean variable,
@@ -72,36 +56,6 @@ function keyPressed() {
     drawSegments = !drawSegments;
   }
 }
-
-
-
-function calculateImageDrawProperties() {
-  //if the image is wider than the canvas
-  if (imgDrawProperties.aspect > canvasAspectRatio) {
-    //then we will draw the image to the width of the canvas
-    imgDrawProperties.width = width;
-    //and calculate the height based on the aspect ratio
-    imgDrawProperties.height = width / imgDrawProperties.aspect;
-    imgDrawProperties.yOffset = (height - imgDrawProperties.height) / 2;
-    imgDrawProperties.xOffset = 0;
-  } else if (imgDrawProperties.aspect < canvasAspectRatio) {
-    //otherwise we will draw the image to the height of the canvas
-    imgDrawProperties.height = height;
-    //and calculate the width based on the aspect ratio
-    imgDrawProperties.width = height * imgDrawProperties.aspect;
-    imgDrawProperties.xOffset = (width - imgDrawProperties.width) / 2;
-    imgDrawProperties.yOffset = 0;
-  }
-  else if (imgDrawProperties.aspect == canvasAspectRatio) {
-    //if the aspect ratios are the same then we can draw the image to the canvas size
-    imgDrawProperties.width = width;
-    imgDrawProperties.height = height;
-    imgDrawProperties.xOffset = 0;
-    imgDrawProperties.yOffset = 0;
-  }
-}
-
-
 
 
 //Here is our class for the image segments, we start with the class keyword

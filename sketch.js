94fadb992656d3680665e3afeefe102657169efd
catiradocortes <carlostiradocortes@gmail.com>
2025-04-17 11:41:26 +1000

    FINISHED PART 3

--- sketch.js
+++ sketch.js
@@ -27,8 +27,7 @@ function setup() {
   // ! calculate the aspect ratio
   imgDrawProperties.aspect = img.width / img.height;
 
-  //! canvas aspect ratio
-  canvasAspectRatio = width/height;
+
   //! Calculate the aspect ratio fo the canvas
   calculateImageDrawProperties();
 
@@ -38,16 +37,23 @@ function setup() {
   /*
   Divide the original image into segments, we are going to use nested loops
   */
-
+  let positionInColumn = 0;
   for (let segYPos=0; segYPos<img.height; segYPos+=segmentHeight) {
     //this is looping over the height
+    let positionInRow = 0;
     for (let segXPos=0; segXPos<img.width; segXPos+=segmentWidth) {
       //We will use the x and y position to get the colour of the pixel from the image
       //lets take it from the centre of the segment
       let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
-      let segment = new ImageSegment(segXPos,segYPos,segmentWidth,segmentHeight,segmentColour);
+      let segment = new ImageSegment(positionInColumn,positionInRow, segmentColour);
       segments.push(segment);
+      positionInRow++;
     }
+    positionInColumn++;
+  }
+
+  for (const segment of segments) {
+    segment.calculateSegDrawProps();
   }
 }
 
@@ -76,6 +82,10 @@ function keyPressed() {
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
   calculateImageDrawProperties();
+
+  for (const segment of segments) {
+    segment.calculateSegDrawProps();
+  }
 }
 
 function calculateImageDrawProperties() {
@@ -114,20 +124,55 @@ function calculateImageDrawProperties() {
 //Here is our class for the image segments, we start with the class keyword
 class ImageSegment {
 
-  constructor(srcImgSegXPosInPrm,srcImgSegYPosInPrm,srcImgSegWidthInPrm,srcImgSegHeightInPrm,srcImgSegColourInPrm) {
-    //these parameters are used to set the internal properties of an instance of the segment
-    //These parameters are named as imageSource as they are derived from the image we are using
-    this.srcImgSegXPos = srcImgSegXPosInPrm;
-    this.srcImgSegYPos = srcImgSegYPosInPrm;
-    this.srcImgSegWidth = srcImgSegWidthInPrm;
-    this.srcImgSegHeight = srcImgSegHeightInPrm;
+  constructor(columnPositionInPrm, rowPostionInPrm  ,srcImgSegColourInPrm) {
+    //Now we have changed the class a lot, instead of the x and y position of the segment, we will store the row and column position
+    //The row and column position give us relative position of the segment in the image that do not change when the image is resized
+    //We will use these to calculate the x and y position of the segment when we draw it
+
+    this.columnPosition = columnPositionInPrm;
+    this.rowPostion = rowPostionInPrm;
     this.srcImgSegColour = srcImgSegColourInPrm;
+    //These parameters are not set when we create the segment object, we will calculate them later
+    this.drawXPos = 0;
+    this.drawYPos = 0;
+    this.drawWidth = 0;
+    this.drawHeight = 0;
+  
+    
   }
+
+
+  calculateSegDrawProps() {
+    //Here is where we will calculate the draw properties of the segment.
+    //The width and height are easy to calculate, remember the image made of segments is always the same size as the whole image even when it is resized
+    //We can use the width and height we calculated for the image to be drawn, to calculate the size of each segment
+    this.drawWidth = imgDrawProperties.width / numSegments;
+    this.drawHeight = imgDrawProperties.height / numSegments;
+    
+    //we can use the row and column position to calculate the x and y position of the segment
+    //Here is a diagram to help you visualise what is going on
+    
+    //          column0 column1 column2 column3 column4
+    //             ↓       ↓       ↓       ↓       ↓
+    //    row0 → 0,0     0,1     0,2     0,3     0,4
+    //    row1 → 1,0     1,1     1,2     1,3     1.4
+    //    row2 → 2,0     2,1     2,2     2,3     2,4
+    //    row3 → 3,0     3,1     3,2     3,3     3,4
+    //    row4 → 4,0     4,1     4,2     4,3     4,4
+
+    //The x position is the row position multiplied by the width of the segment plus the x offset we calculated for the image
+    this.drawXPos = this.rowPostion * this.drawWidth + imgDrawProperties.xOffset;
+    //The y position is the column position multiplied by the height of the segment plus the y offset we calculated for the image
+    this.drawYPos = this.columnPosition * this.drawHeight + imgDrawProperties.yOffset;
+  }
+
+
 
   draw() {
     //lets draw the segment to the canvas, for now we will draw it as an empty rectangle so we can see it
     stroke(0);
     fill(this.srcImgSegColour);
-    rect(this.srcImgSegXPos, this.srcImgSegYPos, this.srcImgSegWidth, this.srcImgSegHeight);
+    //Then draw the segment as a rectangle, using the draw properties we calculated
+    rect(this.drawXPos, this.drawYPos, this.drawWidth, this.drawHeight);
   }
 }
